f87cde3b532429d836aae0e3c1f5400497be7f87
nvan1710 <165784078+nvan1710@users.noreply.github.com>
2025-02-13 19:36:16 +0700

    update code

---
+++ chess-game-backend-api/chess-game-backend-api/src/main/java/vn/btec/chess_game_backend_api/ChessGameBackendApiApplication.java
@@ -0,0 +1,13 @@
+package vn.btec.chess_game_backend_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessGameBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessGameBackendApiApplication.class, args);
+	}
+
+}
---
+++ chess-game-backend-api/chess-game-backend-api/src/test/java/vn/btec/chess_game_backend_api/ChessGameBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.btec.chess_game_backend_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessGameBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
